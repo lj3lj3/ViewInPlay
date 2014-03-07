f67ebd8444655bbb7726bd1a96652a10bade9c2a
DayLemK <lj3lj3@gmail.com>
2014-03-07 17:24:47 +0800

    moded:the double click action on the RecentTask

--- src/info/daylemk/viewinplay/RecentTaskHook.java
+++ src/info/daylemk/viewinplay/RecentTaskHook.java
@@ -6,16 +6,23 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.ResolveInfo;
+import android.content.res.Resources;
 import android.content.res.XModuleResources;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.Settings;
+import android.view.GestureDetector;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 import android.widget.PopupMenu;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
@@ -35,17 +42,24 @@ public class RecentTaskHook {
     // static String TEXT_OPEN_IN_HALO;
     static String TEXT_REMOVE_FROM_LIST;
     static String TEXT_VIEW_IN_PLAY;
+    static String TEXT_NO_PLAY;
 
     static final int ID_REMOVE_FROM_LIST = 1000;
     static final int ID_APP_INFO = 2000;
     // static final int ID_OPEN_IN_HALO = 3000;
     static final int ID_VIEW_IN_PLAY = 4000;
+    
+    static int id_app_thumbnail = -1;
+    
+    static GestureDetector mGestureDetector;
+    static GestureListener mGestureListener;
 
     public static void initZygote(XModuleResources module_res) {
         TEXT_APP_INFO = module_res.getString(R.string.recents_app_info);
         // TEXT_OPEN_IN_HALO = module_res.getString(R.string.recents_open_halo);
         TEXT_REMOVE_FROM_LIST = module_res.getString(R.string.recents_remove_from_list);
         TEXT_VIEW_IN_PLAY = module_res.getString(R.string.view_in_play);
+        TEXT_NO_PLAY = module_res.getString(R.string.no_play_on_the_phone);
     }
 
     public static void handleLoadPackage(final LoadPackageParam lpp, final XSharedPreferences pref) {
@@ -70,6 +84,9 @@ public class RecentTaskHook {
                 Common.DEFAULT_SHOW_IN_RECENT_PANEL)) {
             injectMenu(lpp);
         }
+        
+        injectTouchEvents(lpp);
+ 
     }
 
     private static void injectMenu(final LoadPackageParam lpp) {
@@ -186,7 +203,201 @@ public class RecentTaskHook {
             }
         });
     }
+    
+    private static void injectTouchEvents(final LoadPackageParam lpp) {
+        XposedBridge.log(TAG + TAG_CLASS + "in inject touch eventss");
+        final Class<?> hookClass = XposedHelpers.findClass(
+                "com.android.systemui.recent.RecentsVerticalScrollView",
+                lpp.classLoader);
+        XposedBridge.hookAllMethods(hookClass, "update", new XC_MethodHook() {
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                
+                /*
+//              final Class<?> rClass = XposedHelpers.findClass("com.android.systemui.R.id", lpp.classLoader);
+              try {
+                  Class.forName("com.android.systemui.R.id", false, lpp.classLoader);
+              } catch (ClassNotFoundException e1) {
+                  e1.printStackTrace();
+                  XposedBridge.log(TAG + TAG_CLASS + "ERROR:" + "ClassNotFoundException id");
+              }
+              try {
+                  Class.forName("com.android.systemui.R", false, lpp.classLoader);
+              } catch (ClassNotFoundException e1) {
+                  e1.printStackTrace();
+                  XposedBridge.log(TAG + TAG_CLASS + "ERROR:" + "ClassNotFoundException R");
+              }
+              final Class<?> rClass = XposedHelpers.findClass("com.android.systemui.R", lpp.classLoader);
+              int id_app_thumbnail = 0;
+              try {
+                  Class<?>[] classes = rClass.getDeclaredClasses();
+                  XposedBridge.log(TAG + TAG_CLASS + "length :" + classes.length);
+                  
+                  XposedBridge.log(TAG + TAG_CLASS + "length of classes:" + rClass.getClasses().length);
+                  for (int k = 0; k < classes.length; k ++){
+                      XposedBridge.log(TAG + TAG_CLASS + "name:" + classes[k].getName());
+                      if(classes[k].getName() == "com.android.systemui.R$id"){
+                          id_app_thumbnail = classes[k].getDeclaredField("app_thumbnail").getInt(null);
+                          XposedBridge.log(TAG + TAG_CLASS + "get int :" + id_app_thumbnail);
+                      }
+                  }
+              } catch (NoSuchFieldException e) {
+                  // TODO Auto-generated catch block
+                  XposedBridge.log(TAG + TAG_CLASS + "ERROR:" + "NoSuchFieldException");
+                  e.printStackTrace();
+              } catch (IllegalAccessException e) {
+                  // TODO Auto-generated catch block
+                  XposedBridge.log(TAG + TAG_CLASS + "ERROR:" + "IllegalAccessException");
+                  e.printStackTrace();
+              } catch (IllegalArgumentException e) {
+                  // TODO Auto-generated catch block
+                  XposedBridge.log(TAG + TAG_CLASS + "ERROR:" + "IllegalArgumentException");
+                  e.printStackTrace();
+              }
+              
+              XposedBridge.log(TAG + TAG_CLASS + "GOTIT:" + "id_ap_thumbnail:" + id_app_thumbnail);
+              
+              if(id_app_thumbnail != 0){
+                  
+              } else {
+                  
+              }*/
+                
+                final Object thiz = param.thisObject;
+                final ScrollView sv = (ScrollView)thiz;
+                Resources res = sv.getContext().getResources();
+                
+                
+                if(id_app_thumbnail == -1){
+                    id_app_thumbnail = res.getIdentifier("app_thumbnail", "id", "com.android.systemui");
+                    XposedBridge.log(TAG + TAG_CLASS + "identifier 2: " + id_app_thumbnail);
+                }
+                
+                if(mGestureDetector == null){
+                    mGestureListener = new GestureListener();
+                    mGestureDetector = new GestureDetector(sv.getContext(), mGestureListener);
+                }
+                
+                LinearLayout mLinearLayout = null;
+                try {
+                    mLinearLayout = (LinearLayout) thiz.getClass().getDeclaredField("mLinearLayout").get(thiz);
+                } catch (Exception e) {
+                    // User on ICS
+                }
+                if(mLinearLayout != null){
+                    XposedBridge.log(TAG + TAG_CLASS + "in inject touch events, OK");
+                    RecentsOnTouchLis touchListener = new RecentsOnTouchLis();
+                    FrameLayout frameLayout;
+                    for (int i = 0; i < mLinearLayout.getChildCount(); i ++){
+                        XposedBridge.log(TAG + TAG_CLASS + "i = " + i);
+                        frameLayout = (FrameLayout) mLinearLayout.getChildAt(i);
+                        View vTemp = frameLayout.findViewById(id_app_thumbnail);
+                        if(vTemp != null){
+                            vTemp.setOnTouchListener(touchListener);
+//                            vTemp.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View v) {
+//                                Toast.makeText(sv.getContext(), "text", Toast.LENGTH_SHORT).show();
+//                            }
+//                        });
+                        }
+                        XposedBridge.log(TAG + TAG_CLASS + "the view we got is : " + vTemp);
+                        /*frameLayout.setOnClickListener(null);
+                        View RelativeView = frameLayout.getChildAt(0);
+                        XposedBridge.log(TAG + TAG_CLASS + "the layout inside is " + RelativeView);
+                        ViewGroup layout = (ViewGroup)RelativeView;
+                        for (int j = 0; j < layout.getChildCount(); j ++){
+                            View frameView = layout.getChildAt(j);
+                            XposedBridge.log(TAG + TAG_CLASS + "the count : " + j + ", view is" + frameView);
+                            if(frameView instanceof ViewGroup){
+                                XposedBridge.log(TAG + TAG_CLASS + "the layout inside of inside is " + frameView);
+                                ViewGroup layoutGroup = (ViewGroup)frameView;
+                                XposedBridge.log(TAG + TAG_CLASS + "the layout inside of inside of inside is " + layoutGroup);
+//                                layoutGroup.setOnClickListener(null);
+                            }
+                        }*/
+                    }
+                } else {
+                    XposedBridge.log(TAG + TAG_CLASS + "in inject touch events, can't get linearLayout");
+                }
+            }
+        });
+    }
+    
+    private static class RecentsOnTouchLis implements View.OnTouchListener{
 
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            XposedBridge.log(TAG + TAG_CLASS + "on touch, v : " + v + ", events : " + event);
+            XposedBridge.log(TAG + TAG_CLASS + "events, action : " + event.getAction());
+            XposedBridge.log(TAG + TAG_CLASS + "events, pointer count : " + event.getPointerCount());
+            mGestureListener.setView(v);
+            if(mGestureDetector.onTouchEvent(event)){
+                XposedBridge.log(TAG + TAG_CLASS + "EVENT has been recoganized ");
+//                return true;
+            }
+            
+            return true;
+        }
+    }
+    
+    private static class GestureListener extends GestureDetector.SimpleOnGestureListener {
+        private View v;
+        
+        public void setView (View v){
+            this.v = v;
+        }
+        
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            super.onDoubleTap(e);
+            XposedBridge.log(TAG + TAG_CLASS + "events, DoubleTap : " + e);
+            return true;
+        }
+        
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            super.onDoubleTapEvent(e);
+            XposedBridge.log(TAG + TAG_CLASS + "events, DoubleTap ATE : " + e);
+            return true;
+        }
+
+        /*@Override
+        public boolean onDown(MotionEvent e) {
+            super.onDown(e);
+            XposedBridge.log(TAG + TAG_CLASS + "events, Down ATE : " + e);
+            return true; 
+        }*/
+
+        /*@Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            super.onSingleTapUp(e);
+            XposedBridge.log(TAG + TAG_CLASS + "events, Single tap up, ATE : " + e);
+            return true;
+        }*/
+
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            super.onSingleTapConfirmed(e);
+            v.performClick();
+            XposedBridge.log(TAG + TAG_CLASS + "events, Single tap up confirmed. let it go : " + e);
+            return true;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+            // TODO Auto-generated method stub
+            super.onShowPress(e);
+            v.requestFocus();
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            super.onLongPress(e);
+            v.performLongClick();
+            XposedBridge.log(TAG + TAG_CLASS + "events, Long press : " + e);
+        }
+    }
+    
     private static void closeRecentApps(View thiz) {
         if (Build.VERSION.SDK_INT >= 16) {
             try {
@@ -303,8 +514,9 @@ public class RecentTaskHook {
             // if didn't find the Google Play Store, show the toast
             // TODO Toast may can not show by system, so we should do later
             // about it, maybe a broadcast instead
-            // Toast.makeText(ctx, R.string.no_play_on_the_phone,
-            // Toast.LENGTH_LONG).show();
+            // DONE just need to get the resource at init moment
+            Toast.makeText(ctx, TEXT_NO_PLAY,
+                    Toast.LENGTH_LONG).show();
         }
     }
 }
